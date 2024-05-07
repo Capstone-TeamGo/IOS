4a34127132156a91a79011475067c05958a4aef5
jeoungsung12 <jeoungsung12@naver.com>
2024-05-07 17:23:00 +0900

    [Feat] MypageVC init

--- Capstone/Feat/Mypage/MypageViewController.swift
+++ Capstone/Feat/Mypage/MypageViewController.swift
@@ -20,15 +20,66 @@ class MypageViewController: UIViewController{
         image.image = UIImage(named: "appIcon")
         return image
     }()
-    //개인 정보 텍스트
-    private let personText : UITextView = {
-        let view = UITextView()
-        view.backgroundColor = .clear
-        view.textAlignment = .left
-        view.isUserInteractionEnabled = false
-        view.isScrollEnabled = false
-        view.text = "김승진"
+    //이미지
+    private let personImage : UIImageView = {
+        let view = UIImageView()
+        view.backgroundColor = .white
+        view.contentMode = .scaleAspectFit
+        view.image = UIImage(systemName: "person.circle.fill")
+        view.tintColor = .lightGray
         return view
+    }()
+    //개인 정보 텍스트
+    private let nameLabel : UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .white
+        label.text = "김승진"
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.textAlignment = .center
+        return label
+    }()
+    private let emailLabel : UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .white
+        label.text = "Permission@Denied"
+        label.textColor = .gray
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textAlignment = .center
+        return label
+    }()
+    //하위 메뉴
+    private let spacing : UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    private let logoutBtn : UIButton = {
+        let btn = UIButton()
+        btn.backgroundColor = .clear
+        btn.setTitle("로그아웃", for: .normal)
+        btn.setTitleColor(.black, for: .normal)
+        btn.setTitleColor(.gray, for: .highlighted)
+        btn.configuration = .bordered()
+        return btn
+    }()
+    private let listBtn : UIButton = {
+        let btn = UIButton()
+        btn.backgroundColor = .clear
+        btn.setTitle("분석 기록", for: .normal)
+        btn.setTitleColor(.black, for: .normal)
+        btn.setTitleColor(.gray, for: .highlighted)
+        btn.configuration = .bordered()
+        return btn
+    }()
+    private let feedBackBtn : UIButton = {
+        let btn = UIButton()
+        btn.backgroundColor = .clear
+        btn.setTitle("피드백 보내기", for: .normal)
+        btn.setTitleColor(.black, for: .normal)
+        btn.setTitleColor(.gray, for: .highlighted)
+        btn.configuration = .bordered()
+        return btn
     }()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,12 +109,48 @@ extension MypageViewController {
 extension MypageViewController {
     private func setLayout() {
         self.view.backgroundColor = .white
-        self.view.addSubview(personText)
+        self.view.addSubview(personImage)
+        self.view.addSubview(nameLabel)
+        self.view.addSubview(emailLabel)
+        self.view.addSubview(spacing)
+        self.view.addSubview(logoutBtn)
+        self.view.addSubview(listBtn)
+        self.view.addSubview(feedBackBtn)
         
-        personText.snp.makeConstraints { make in
+        personImage.snp.makeConstraints { make in
+            make.height.equalToSuperview().dividedBy(4)
+            make.leading.trailing.equalToSuperview().inset(80)
+            make.top.equalToSuperview().offset(self.view.frame.height / 9)
+        }
+        nameLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(personImage.snp.bottom).offset(10)
+            make.height.equalTo(20)
+        }
+        emailLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(nameLabel.snp.bottom).offset(10)
+            make.height.equalTo(30)
+        }
+        spacing.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview().inset(30)
-            make.top.equalToSuperview().inset(self.view.frame.height / 8)
-            make.height.equalToSuperview().dividedBy(3)
+            make.top.equalTo(emailLabel.snp.bottom).offset(20)
+            make.height.equalTo(0.5)
+        }
+        logoutBtn.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.center.equalToSuperview().offset(80)
+            make.height.equalTo(40)
+        }
+        listBtn.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.top.equalTo(logoutBtn.snp.bottom).offset(20)
+            make.height.equalTo(40)
+        }
+        feedBackBtn.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.top.equalTo(listBtn.snp.bottom).offset(20)
+            make.height.equalTo(40)
         }
     }
 }
